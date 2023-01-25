82b6bff2bcf012a0f2feb10519dfe1e9ee43d89a
Jane-Araza <mary.araza@novare.com.hk>
2023-01-25 22:03:57 +0800

    Initial commit

---
+++ src/main/java/com/example/Task/entity/Task.java
@@ -0,0 +1,54 @@
+package com.example.Task.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "id")
+     private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "completed")
+    private String completed;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(String completed) {
+        this.completed = completed;
+    }
+}
